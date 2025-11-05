6b2aa868efe72bf1725d5d6ba04ecd2110ee2654
KVSREYAS <kvsreyas12@gmail.com>
2025-11-06 02:26:34 +0530

    changed host

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:8080,
+    port: 8080,   // 👈 uses Railway's PORT
+    host: '0.0.0.0',                  // 👈 important so Railway can access it
     proxy: {
       '/api': {
         target: 'https://myapp2-736053828578.asia-south2.run.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: true,
+        secure:true
       },
     },
   },
 })
-
