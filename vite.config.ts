57dc56432bdf6b5c6fbeda405eb6a4bd6885a613
KVSREYAS <kvsreyas12@gmail.com>
2025-11-06 02:28:46 +0530

    allowing hosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 8080,   // 👈 uses Railway's PORT
-    host: '0.0.0.0',                  // 👈 important so Railway can access it
+    host: '0.0.0.0',     
+    allowedHosts: [
+      'cloudprojectfrontend-production.up.railway.app', // 👈 add your Railway domain
+    ],             // 👈 important so Railway can access it
     proxy: {
       '/api': {
         target: 'https://myapp2-736053828578.asia-south2.run.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-        secure:true
       },
     },
   },
